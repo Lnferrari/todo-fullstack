c7cd416e015f820b428349cf181281e780caf76f
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-10-10 18:31:33 +0200

    adding structure to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Registration from './pages/Registration';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App min-h-screen bg-gray-100">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Registration} />
+          {/* <Route />
+          <Route />
+          <Route /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
