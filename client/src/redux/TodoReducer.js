c593cb1cfb1b99f45eb4d0bc8773cc999fa6aa56
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-10-11 16:02:51 +0200

    updating reducer

--- client/src/redux/TodoReducer.js
+++ client/src/redux/TodoReducer.js
@@ -15,28 +15,31 @@ export const TodoReducer = (state = initialState, action) => {
       }
     case 'TOGGLE_TODO':
       const todo = state.todos.find(
-        todo => todo._id === action.payload
+        item => item._id === action.payload._id
       )
-      const updatedTodos = state.todos.map(
+      const toggledTodos = state.todos.map(
         item => item._id === todo._id
         ? item.completed = !item.completed
+        : item
+      )
+      return { ...state, todos: toggledTodos }
+    case 'EDIT_TODO':
+      const editTodo = state.todos.find(
+        item => item._id === action.payload._id
+      )
+      const updatedTodos = state.todos.map(
+        item => item._id === editTodo._id
+        ? action.payload
         : item
       )
       return { ...state, todos: updatedTodos }
-    case 'EDIT_TODO':
-      const todo = state.todos.find(
-        todo => todo._id === action.payload
-      )
-      const updatedTodos = state.todos.map(
-        item => item._id === todo._id
-        ? item.completed = !item.completed
-        : item
-      )
     case 'DELETE_TODO':
       const filteredTodos = state.todos.filter(
-        todo => todo._id !== action.payload 
+        item => item._id !== action.payload._id
       )
       return { ...state, todos: filteredTodos }
+    case 'GET_ALL_TODOS':
+      return { ...state, todos: action.payload }
     default:
       return state
   }
