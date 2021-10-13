424c05c1eb3e3e7f7c90642d663392971faedb48
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-10-13 15:37:46 +0200

    adding auth function, search input and todoList

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
 import { getUserTodos } from '../helpers/apiCalls'
 import AddTodoForm from '../components/dashboard/AddTodoForm'
-import { Box, Grid, List } from '@mui/material'
-import Todo from '../components/dashboard/Todo'
-import { getAllTodos } from '../redux/todoSlice'
+import { Box, Grid, Autocomplete, TextField, Typography } from '@mui/material'
+import SideBar from '../components/dashboard/SideBar'
+import SearchIcon from '@mui/icons-material/Search';
+import { green, grey } from '@mui/material/colors';
+import { setTodos } from '../redux/todoSlice'
+import { setUser } from '../redux/userSlice';
+import { authenticateUser } from '../helpers/apiCalls';
+import TodoList from '../components/dashboard/TodoList';
+import Clock from '../components/dashboard/Clock'
+
 
 const Dashboard = () => {
   const todos = useSelector(state => state.todos)
@@ -12,51 +20,93 @@ const Dashboard = () => {
   const dispatch = useDispatch()
   console.log('state', user)
 
-  const userTodosMarkUp = user?.todos?.map(
-    todo => (
-      <Todo key={todo._id} todo={todo} />
-    )
-  )
-
+  let history = useHistory()
+  
   useEffect(() => {
-    const fetchData = async () => {
-      const todosApi = await getUserTodos(user._id)
-      console.log(todosApi)
-      dispatch(
-        getAllTodos(todosApi)
-      )
+    const authMe = async () => {
+      try {
+        const resApi = await authenticateUser()
+        if (resApi.error) {
+          history.push('/')
+        }
+        dispatch(setUser(resApi));
+        const todosApi = await getUserTodos(resApi._id)
+        console.log('todosAPI =>' , resApi._id)
+        dispatch(
+          setTodos(todosApi)
+        )
+      } catch (err) {
+        console.log('[ERROR]', err.message)
+      }
     }
-    fetchData()
+    authMe()
   }, [])
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const todosApi = await getUserTodos(user._id)
+  //     console.log('todosAPI =>' , todosApi)
+  //     dispatch(
+  //       setTodos(todosApi)
+  //     )
+  //   }
+  //   fetchData()
+  // }, [user])
+
 
   return (
     <Box
       sx={{
+        minWidth: '100vw',
+        minHeigth: '100vh',
         display: 'grid',
-        gridTemplateColumns: '300px 1fr 240px',
-        gap: 1,
-        gridTemplateAreas: `"sidebar header header"
+        gridTemplateColumns: '300px 1fr 255px',
+        gridTemplateAreas: `"sidebar header chat"
         "sidebar content chat"
         "sidebar content chat"`,
       }}
     >
-      <Box sx={{ gridArea: 'header', bgcolor: 'primary.main' }}>
-        Header
+      <Box sx={{ gridArea: 'header', bgcolor: grey[900], p: 2, display: 'flex', justifyContent: 'space-between' }}>
+        <Clock />
+        <Autocomplete
+          freeSolo
+          id="free-solo-2-demo"
+          options={todos?.map((option) => option.title)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='Search Task'
+              sx={{ width: 350, bgcolor: grey[200] }}
+              size='small'
+              InputProps={{
+                ...params.InputProps,
+                type: 'search',
+              }}
+            />
+          )}
+        />
       </Box>
-      <Box sx={{ gridArea: 'sidebar', bgcolor: 'secondary.main' }}>
-        Sidebar
+      <Box sx={{
+        gridArea: 'sidebar',
+        bgcolor: grey[900],
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingY: 2,
+      }}>
+        <SideBar />
       </Box>
-      <Box sx={{ gridArea: 'content', bgcolor: 'info.main', padding: 3 }}
+      <Box sx={{ gridArea: 'content', bgcolor: grey[800], padding: 3 }}
         container
         direction="column"
         alignItems="stretch"  
       >
         <AddTodoForm />
         <Grid fullWidth sx={{ display: 'flex', flexWrap: 'wrap' }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {userTodosMarkUp}
+          <TodoList />
         </Grid>
       </Box>
-      <Box sx={{ gridArea: 'chat', bgcolor: 'warning.main' }}>Chat</Box>
+      <Box sx={{ gridArea: 'chat', bgcolor: grey[900] }}>Chat</Box>
     </Box>
   )
 }
